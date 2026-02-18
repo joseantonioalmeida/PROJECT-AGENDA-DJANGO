d9c0e01b079b065bd176c9d47c3aea83ecc5c53a
Jose Antonio <joseantonioalmeida217@gmail.com>
2026-02-18 12:07:16 -0300

    customizando as opcoes admin.ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -5,4 +5,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id','first_name', 'last_name', 'phone', 'email', 'create_date',)
+    ordering = ('-id',)
+    # list_filter = ('create_date',)
+    search_fields = ('id','first_name','last_name', 'phone', 'email')
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = ('first_name',) #informar quais campos podem ser editados
+    list_display_links = ('id','phone',)# exiba o link onde eu quero
