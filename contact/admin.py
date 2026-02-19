52c970135cf73f8e86bc6684209a5a615fa95a43
Jose Antonio <joseantonioalmeida217@gmail.com>
2026-02-19 17:07:06 -0300

    Manipulando QuerySets Django com filter, order_by e fatiamento

--- contact/admin.py
+++ contact/admin.py
@@ -5,13 +5,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name', 'last_name', 'phone', 'email', 'create_date',)
+    list_display = ('id','first_name', 'last_name', 'phone', 'email', 'create_date', 'show',)
     ordering = ('-id',)
     # list_filter = ('create_date',)
     search_fields = ('id','first_name','last_name', 'phone', 'email')
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = ('first_name',) #informar quais campos podem ser editados
+    list_editable = ('first_name','show',) #informar quais campos podem ser editados
     list_display_links = ('id','phone',)# exiba o link onde eu quero
 
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -4,12 +4,16 @@ from contact import models
 # Create your views here.
 
 def index(request):
-    contacts = models.Contact.objects.all()
+    contacts = models.Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[10:30]
+    
+    # QuerySets
+    # print(contacts.query)
 
     context = {
         'contacts': contacts,
-    }
-    
+    }    
     return render(
         request,
         'contact/index.html',
